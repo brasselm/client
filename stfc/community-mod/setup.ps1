3f4e7a1cb740a84966d9deb74f4b49792e1746a3
brasselm <markus@brassel.me>
2024-02-23 15:47:38 +0100

    fix game path

--- stfc/community-mod/setup.ps1
+++ stfc/community-mod/setup.ps1
@@ -1,7 +1,7 @@
 #Requires -RunAsAdministrator
 $game_path = "C:\Games\Star Trek Fleet Command\STFC\default\game"
 if (-not (Test-Path $game_path\prime.exe)) {
-    $game_path = Get-Childitem –Path C:\prime.exe -Recurse -ErrorAction SilentlyContinue | select -First 1 | select Directory
+    $game_path = Get-Childitem –Path C:\prime.exe -Recurse -ErrorAction SilentlyContinue | select -First 1 | select -ExpandProperty Directory
 }
 $sourcepath = $Script:PSScriptRoot
 if ( -not (Test-Path $sourcepath\version.dll)) {
