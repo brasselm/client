d05517e6dd656dc303f62a4a4025737791046c9c
brasselm <markus@brassel.me>
2024-02-23 15:40:31 +0100

    init stfc

---
+++ stfc/community-mod/setup.ps1
@@ -0,0 +1,15 @@
+#Requires -RunAsAdministrator
+$game_path = "C:\Games\Star Trek Fleet Command\STFC\default\game"
+if (-not (Test-Path $game_path\prime.exe)) {
+    $game_path = Get-Childitem –Path C:\prime.exe -Recurse -ErrorAction SilentlyContinue | select -First 1 | select Directory
+}
+$sourcepath = $Script:PSScriptRoot
+if ( -not (Test-Path $sourcepath\version.dll)) {
+    $sourcepath = $PWD
+}
+elseif ( -not (Test-Path $sourcepath\version.dll)) {
+}
+else {
+    New-Item -Path $game_path\version.dll -ItemType SymbolicLink -Value $sourcepath\version.dll -Force
+    New-Item -Path $game_path\community_patch_settings.toml -ItemType SymbolicLink -Value $sourcepath\community_patch_settings.toml -Force
+}
